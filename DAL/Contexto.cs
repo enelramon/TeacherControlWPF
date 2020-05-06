9b046ca85570dfed8a8558e612ccb17bf279c48d
Enel Almonte <enelramon@hotmail.com>
2020-05-06 10:43:07 -0400

    Agregando Contexto y Migración

---
+++ DAL/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeacherControlWPF.Entidades;
+
+namespace TeacherControlWPF.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Nacionalidades> Nacionalidades { get; set; }
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\TeacherControl.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Nacionalidades>().HasData(new Nacionalidades { NacionalidadId=1,Nacionalidad="Dominicana"});
+
+        }
+
+    }
+}
