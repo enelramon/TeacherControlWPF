9548228b7acdf551ca89c463c75addfdc2964dc4
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-05-06 10:22:37 -0400

    Creacion de Registro Estudiante

---
+++ UI/Registro/REstudiante.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeacherControlWPF.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for REstudiante.xaml
+    /// </summary>
+    public partial class REstudiante : Window
+    {
+        public REstudiante()
+        {
+            InitializeComponent();
+        }
+    }
+}
