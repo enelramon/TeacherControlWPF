db19f6fa05ecf3c5c673f25335d792fcb0c45c4e
Enel Almonte <enelramon@hotmail.com>
2020-05-06 10:36:30 -0400

    Agregando Entidades

---
+++ Entidades/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TeacherControlWPF.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre")]
+        public string Nombres { get; set; }
+
+        [Range(minimum: 1, maximum: 10, ErrorMessage = "Seleccione un semestre")]
+        public int Semestre { get; set; }
+
+        public int NacionalidadId { get; set; }
+
+        [ForeignKey("NacionalidadId")]
+        public virtual Nacionalidades Nacionalidad { get; set; }
+    }
+}
---
+++ Entidades/Nacionalidades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeacherControlWPF.Entidades
+{
+    public class Nacionalidades
+    {
+        [Key]
+        public int NacionalidadId { get; set; }
+        public string Nacionalidad { get; set; }
+    }
+}
