d6173f34b9fe6ade0e5299b5be8df50ff2070c2d
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-05-07 19:26:18 -0400

    Modificaciones en la Ventana Principal y en rAdicionales junto con las consultas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TeacherControlWPF.UI.Consultas;
 using TeacherControlWPF.UI.Registro;
 
 namespace TeacherControlWPF
@@ -26,10 +27,40 @@ namespace TeacherControlWPF
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+       
+        private void EstudianteMenuItem_Click(object sender, RoutedEventArgs e)
         {
             rEstudiantes re = new rEstudiantes();
             re.Show();
+        }
+
+        private void AdicionalesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            rAdicionales ra = new rAdicionales();
+            ra.Show();
+        }
+
+        private void TareasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            rTareas rt = new rTareas();
+            rt.Show();
+        }
+
+        private void ConsultaEstudianteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            cEstudiantes ce = new cEstudiantes();
+            ce.Show();
+
+        }
+
+        private void ConsultaAdicionalesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultaTareasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
