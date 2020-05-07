47dd8fa76a13511a3cca7f7fa6ec04112014d375
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-05-06 21:40:58 -0400

    Codigos de los botones

--- UI/Registro/rEstudiantes.xaml.cs
+++ UI/Registro/rEstudiantes.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TeacherControlWPF.BLL;
 using TeacherControlWPF.Entidades;
 
 namespace TeacherControlWPF.UI.Registro
@@ -25,29 +26,83 @@ namespace TeacherControlWPF.UI.Registro
             InitializeComponent();
 
             this.DataContext = Estudiante;
-
             //Cargar las nacionalidades al ComboBox
 
+            EstudianteIdTextBox.Text = "0"; //Se inicializa el Textbox en 0
+
+        }
+            
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = Estudiante;
+        }
+
+        private void Limpiar()
+        {
+            EstudianteIdTextBox.Text = "0";
+            FechaIngresoDataPicker.Text = string.Empty;
+            NombresTextBox.Text = string.Empty;
+            SemestreTextBox.Text = string.Empty;
+            PuntosExtrasTextBox.Text = string.Empty;
+            NacionalidadesComboBox.Text = string.Empty;
+            this.Estudiante = new Estudiantes();
+            Cargar();
         }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Estudiante = new Estudiantes();
+            Limpiar();
+            
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Estudiantes e = EstudiantesBLL.Buscar(Estudiante.EstudianteId);
+
+            return (e != null);
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            bool paso = false;
 
+            if (Estudiante.EstudianteId == 0)
+            {
+                paso = EstudiantesBLL.Guardar(Estudiante);
+            }
+            else
+            {
+                if (ExisteEnLaBaseDeDatos())
+                {
+                    paso = EstudiantesBLL.Modificar(Estudiante);
+                }
+                else
+                {
+                    MessageBox.Show("No existe en la Base de Datos","ERROR");
+                    return;
+                }
+
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("Guardado!!", "Exito");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo Guardar","ERROR");
+                }
+            }
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (EstudiantesBLL.Eliminar)
         }
 
     }
