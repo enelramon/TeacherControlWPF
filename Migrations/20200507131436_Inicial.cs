326fce8afa5ff110952907d5d07b7c6025337ce2
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-05-07 09:19:03 -0400

    Modificaciones en las entidades Tareas y Tareas detalle, y actualizando migracion y base de datos

---
+++ Migrations/20200507131436_Inicial.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeacherControlWPF.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adicionales",
+                columns: table => new
+                {
+                    AdicionalId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    EstudianteId = table.Column<int>(nullable: false),
+                    Puntos = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adicionales", x => x.AdicionalId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nacionalidades",
+                columns: table => new
+                {
+                    NacionalidadId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nacionalidad = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nacionalidades", x => x.NacionalidadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    TareaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Puntos = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.TareaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    EstudianteId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FechaIngreso = table.Column<DateTime>(nullable: false),
+                    Nombres = table.Column<string>(nullable: true),
+                    Semestre = table.Column<int>(nullable: false),
+                    PuntosExtra = table.Column<float>(nullable: false),
+                    NacionalidadId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.EstudianteId);
+                    table.ForeignKey(
+                        name: "FK_Estudiantes_Nacionalidades_NacionalidadId",
+                        column: x => x.NacionalidadId,
+                        principalTable: "Nacionalidades",
+                        principalColumn: "NacionalidadId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TareasDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TareaId = table.Column<int>(nullable: false),
+                    Requerimiento = table.Column<string>(nullable: true),
+                    Valor = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TareasDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TareasDetalle_Tareas_TareaId",
+                        column: x => x.TareaId,
+                        principalTable: "Tareas",
+                        principalColumn: "TareaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Nacionalidades",
+                columns: new[] { "NacionalidadId", "Nacionalidad" },
+                values: new object[] { 1, "Dominicana" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estudiantes_NacionalidadId",
+                table: "Estudiantes",
+                column: "NacionalidadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TareasDetalle_TareaId",
+                table: "TareasDetalle",
+                column: "TareaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adicionales");
+
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+
+            migrationBuilder.DropTable(
+                name: "TareasDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Nacionalidades");
+
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
--- UI/Registro/rTareas.xaml.cs
+++ UI/Registro/rTareas.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TeacherControlWPF.Entidades;
+using TeacherControlWPF.DAL;
 
 namespace TeacherControlWPF.UI.Registro
 {
@@ -17,9 +19,56 @@ namespace TeacherControlWPF.UI.Registro
     /// </summary>
     public partial class rTareas : Window
     {
+        private Tareas Tarea = new Tareas();
+
         public rTareas()
         {
             InitializeComponent();
+            this.DataContext = Tarea;
         }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = Tarea;
+        }
+
+        private void Limpiar()
+        {
+            this.Tarea = new Tareas();
+            this.DataContext = Tarea;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
