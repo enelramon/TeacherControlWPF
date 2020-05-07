83bb50d68cffdffa3aeddf1a479570bcc75f6c39
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-05-06 21:52:11 -0400

    Creando BLL de la entidad tareas

---
+++ BLL/TareasBLL.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeacherControlWPF.Entidades;
+using TeacherControlWPF.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TeacherControlWPF.BLL
+{
+    public class TareasBLL
+    {
+        /// <summary>
+        /// Permite guardar una entidad en la base de datos
+        /// </summary>
+        /// <param name="tarea">La entidad que se desea guardar</param>
+        public static bool Guardar(Tareas tarea)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                //Agregar la entidad que se desea insertar al contexto
+                contexto.Tareas.Add(tarea);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        /// <summary>
+        /// Permite modificar una entidad en la base de datos
+        /// </summary>
+        /// <param name="tarea">La entidad que se desea modificar</param>
+        public static bool Modificar(Tareas tarea)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete FROM TareasDetalle Where TareaId={tarea.TareaId}");
+                foreach(var item in tarea.Detalle)
+                {
+                    contexto.Entry(item).State = EntityState.Added;
+                }
+                contexto.Entry(tarea).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        /// <summary>
+        /// Permite eliminar una entidad de la base de datos
+        /// </summary>
+        /// <param name="id">El Id de la entidad que se desea eliminar</param> 
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var eliminar = TareasBLL.Buscar(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        /// <summary>
+        /// Permite buscar una entidad en la base de datos
+        /// </summary>
+        /// <param name="id">El Id de la entidad que se desea buscar</param>
+        public static Tareas Buscar(int id)
+        {
+            Tareas tarea = new Tareas();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                tarea = contexto.Tareas.Include(x => x.Detalle)
+                    .Where(x => x.TareaId == id)
+                    .SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return tarea;
+        }
+
+        /// <summary>
+        /// Permite obtener una lista filtrada por un criterio de busqueda
+        /// </summary>
+        /// <param name="criterio">La expresión que define el criterio de busqueda</param>
+        /// <returns></returns>
+        public static List<Tareas> GetList(Expression<Func<Tareas, bool>> criterio)
+        {
+            List<Tareas> Lista = new List<Tareas>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                //obtener la lista y filtrarla según el criterio recibido por parametro.
+                Lista = contexto.Tareas.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
