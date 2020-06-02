21a0bb4922ea10950893b009625f5e34c3cae9b1
Enel Almonte <enelramon@hotmail.com>
2020-06-01 20:46:50 -0400

    Agregando Tests

---
+++ TeacherControlWPF.Tests/BLL/EstudiantesBLLTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeacherControlWPF.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherControlWPF.BLL.Tests
+{
+    public class EstudiantesBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
